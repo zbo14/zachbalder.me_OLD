aa92f22b376c01b11d380c0a6aac7a2779a9ff63
Zach Balder <zach@wisiwig.co>
2023-09-30 19:20:30 -0400

    favicon for now

--- .eleventy.js
+++ .eleventy.js
@@ -2,7 +2,7 @@ const sitemap = require('@quasibit/eleventy-plugin-sitemap')
 const webc = require('@11ty/eleventy-plugin-webc')
 
 module.exports = function (eleventyConfig) {
-  // eleventyConfig.addPassthroughCopy('assets')
+  eleventyConfig.addPassthroughCopy('assets')
   // eleventyConfig.addPassthroughCopy('**/*.css')
   // eleventyConfig.addPassthroughCopy('**/*.js')
 
